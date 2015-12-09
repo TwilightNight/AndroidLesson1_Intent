c34aa5fdee71e40dabf095b9e75d17d0b3e8e7a6
Twilight <rie00002@gmail.com>
2015-12-10 00:09:01 +0800

    Add ButterKnife

--- app/src/main/java/com/example/shana/chat/ChatActivity.java
+++ app/src/main/java/com/example/shana/chat/ChatActivity.java
@@ -3,41 +3,37 @@ package com.example.shana.chat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by shana on 2015/12/9.
  */
 public class ChatActivity extends Activity {
     public static final String CHAT_ACTIVITY_WORD_KEY = "CHAT_ACTIVITY_WORD_KEY";
-    public static final int CHAT_ACTIVITY_SUCCESS_CODE = 0;
+    public static final int CHAT_ACTIVITY_SUCCESS_CODE = 777;
+
+    @Bind(R.id.activity_main_text)
+    protected TextView textView;
+
+    @Bind(R.id.activity_main_edit)
+    protected EditText editText;
+
+    @OnClick(R.id.activity_main_button)
+    protected void onSendMessage(){
+        finishPageWithMessage(editText.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTextViewMessage(getIntent().getStringExtra(CHAT_ACTIVITY_WORD_KEY));
-
-        Button button = (Button) findViewById(R.id.activity_main_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finishPageWithMessage(getMessageFromEditText());
-            }
-        });
-    }
-
-    private void setTextViewMessage(String message) {
-        TextView text = (TextView) findViewById(R.id.activity_main_text);
-        text.setText(message);
-    }
-
-    private String getMessageFromEditText() {
-        EditText edit = (EditText) findViewById(R.id.activity_main_edit);
-        return edit.getText().toString();
+        ButterKnife.bind(this);
+        textView.setText((getIntent().getStringExtra(CHAT_ACTIVITY_WORD_KEY)));
     }
 
     private void finishPageWithMessage(String message) {
--- app/src/main/java/com/example/shana/chat/MainActivity.java
+++ app/src/main/java/com/example/shana/chat/MainActivity.java
@@ -3,36 +3,33 @@ package com.example.shana.chat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by shana on 2015/12/9.
  */
 public class MainActivity extends Activity {
+    @Bind(R.id.activity_main_text)
+    protected TextView textView;
+
+    @Bind(R.id.activity_main_edit)
+    protected EditText editText;
+
+    @OnClick(R.id.activity_main_button)
+    protected void onSendMessage(){
+        sendMessageToNextPage(editText.getText().toString());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.activity_main_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMessageToNextPage(getMessageFromEditText());
-            }
-        });
-    }
-
-    private void setTextViewMessage(String message) {
-        TextView text = (TextView) findViewById(R.id.activity_main_text);
-        text.setText(message);
-    }
-
-    private String getMessageFromEditText(){
-        EditText edit = (EditText) findViewById(R.id.activity_main_edit);
-        return edit.getText().toString();
+        ButterKnife.bind(this);
     }
 
     private void sendMessageToNextPage(String message){
@@ -45,7 +42,7 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == ChatActivity.CHAT_ACTIVITY_SUCCESS_CODE) {
-            setTextViewMessage(data.getStringExtra(ChatActivity.CHAT_ACTIVITY_WORD_KEY));
+            textView.setText(data.getStringExtra(ChatActivity.CHAT_ACTIVITY_WORD_KEY));
         }
     }
 }
