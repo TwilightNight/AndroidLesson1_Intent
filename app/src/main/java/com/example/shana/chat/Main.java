30d0174b59505e29313f0736cb5c95a8c67f2030
Twilight <rie00002@gmail.com>
2015-12-09 14:22:39 +0800

    Initial Main Page

---
+++ app/src/main/java/com/example/shana/chat/Main.java
@@ -0,0 +1,15 @@
+package com.example.shana.chat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shana on 2015/12/9.
+ */
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
